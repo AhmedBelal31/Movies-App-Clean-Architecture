5b71d44094f52a640114761d2cdc432d53bde369
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-02-26 04:56:24 +0200

    Create Pagination For Top Rated Movies

--- lib/modules/movies/presentation/screens/all_top_rated_movies_screen.dart
+++ lib/modules/movies/presentation/screens/all_top_rated_movies_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app_clean_architecture/core/services/service_locator.dart';
+import 'package:movies_app_clean_architecture/modules/movies/domain/entity/movie_entity.dart';
 import 'package:movies_app_clean_architecture/modules/movies/presentation/controller/movies_cubit/movies_cubit.dart';
 import 'package:movies_app_clean_architecture/modules/movies/presentation/controller/movies_cubit/movies_states.dart';
 import 'package:movies_app_clean_architecture/modules/movies/presentation/widgets/all_movies_item.dart';
@@ -14,11 +15,43 @@ class AllTopRatedMovies extends StatefulWidget {
 }
 
 class _AllTopRatedMoviesState extends State<AllTopRatedMovies> {
+  late final ScrollController _scrollController;
+
+  bool isLoading = false;
+
+  int nextPage = 1;
+
   @override
   void initState() {
-
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      return addListenToListState();
+    });
     super.initState();
   }
+
+  void addListenToListState() async {
+    var currentPosition = _scrollController.position.pixels;
+    var maxScrollLength = _scrollController.position.maxScrollExtent;
+    if (currentPosition >= maxScrollLength * 0.7) {
+      print('iam 70% here ');
+      if (!isLoading) {
+        isLoading = true;
+        await BlocProvider.of<MoviesCubit>(context)
+            .getTopRatedMovies(pageNumber: nextPage);
+        nextPage++;
+        print('Page is $nextPage');
+        isLoading = false;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,22 +62,21 @@ class _AllTopRatedMoviesState extends State<AllTopRatedMovies> {
       ),
       body: BlocBuilder<MoviesCubit, MoviesStates>(
         builder: (context, state) {
-          var moviesCubit = BlocProvider.of<MoviesCubit>(context);
-          if (moviesCubit.topRatedMovies.isNotEmpty) {
+          // print('List $allTopRatedMovies');
+          var topRatedMovies =
+              BlocProvider.of<MoviesCubit>(context).topRatedMovies;
+          if (topRatedMovies.isNotEmpty) {
             return ListView.separated(
+              controller: _scrollController,
               physics: const BouncingScrollPhysics(),
               itemBuilder: (context, index) =>
-                  AllMoviesItem(movie: moviesCubit.topRatedMovies[index]),
-              separatorBuilder: (context, index) =>
-              const SizedBox(height: 10),
-              itemCount: 8,
+                  AllMoviesItem(movie: topRatedMovies[index]),
+              separatorBuilder: (context, index) => const SizedBox(height: 10),
+              itemCount: topRatedMovies.length,
             );
-
-          }
-          else if (state is TopRatedMoviesFailureState) {
+          } else if (state is TopRatedMoviesFailureState) {
             return Center(child: Text('Error , ${state.errorMessage}'));
-          }
-          else {
+          } else {
             return allMoviesItemLoading();
           }
         },
@@ -52,5 +84,3 @@ class _AllTopRatedMoviesState extends State<AllTopRatedMovies> {
     );
   }
 }
-
-
